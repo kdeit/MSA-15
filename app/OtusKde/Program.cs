2a5f3af42122e084be440f89b0b9b5fd8816d638
Dmitry.Kuznecov <dimakuzn@gmail.com>
2024-09-26 17:28:38 +0300

    add prometheus

--- app/OtusKde/Program.cs
+++ app/OtusKde/Program.cs
@@ -1,5 +1,6 @@
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
+using OpenTelemetry.Metrics;
 using OtusKdeDAL;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,6 +30,18 @@ builder.Services.AddDbContext<MasterContext>(
     opt => opt.UseNpgsql(connectionString)
 );
 
+builder.Services.AddOpenTelemetry().WithMetrics(builder =>
+{
+    builder.AddPrometheusExporter();
+    builder.AddMeter("Microsoft.AspNetCore.Hosting");
+    builder.AddView("http.server.request.duration",
+        new ExplicitBucketHistogramConfiguration
+        {
+            Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05,
+                0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -40,4 +53,5 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.MapControllers();
+app.MapPrometheusScrapingEndpoint();
 app.Run();
