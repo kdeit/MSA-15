8d0d2b891a0567920f198b28ebda258e2e795da5
Dmitry.Kuznecov <dimakuzn@gmail.com>
2024-09-26 10:31:49 +0300

    seeder: add configs

--- app/OtusKdeSeeder/OtusKdeSeeder/Program.cs
+++ app/OtusKdeSeeder/OtusKdeSeeder/Program.cs
@@ -4,9 +4,17 @@ using OtusKde.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var DB_HOST = Environment.GetEnvironmentVariable("DB_HOST");
+var DB_PORT = Environment.GetEnvironmentVariable("DB_PORT");
+var DB_NAME = Environment.GetEnvironmentVariable("DB_NAME");
+var DB_USER = Environment.GetEnvironmentVariable("DB_USER");
+var DB_PASSWORD = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+Console.Write($"DB params: {DB_HOST} | {DB_PORT} | {DB_NAME} | {DB_USER} | {DB_PASSWORD} ");
+
 builder.Services.AddDbContext<MasterContext>(
     opt => 
-        opt.UseNpgsql("Host=localhost;Database=kdeOtusDb;Username=postgres;Password=docker;Port=5432")
+        opt.UseNpgsql($"Host={DB_HOST};Database={DB_NAME};Username={DB_USER};Password={DB_PASSWORD};Port={DB_PORT}")
 );
 
 var app = builder.Build();
