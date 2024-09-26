5be622e465edc672a7fbd31980b3e584cc0b299d
Dmitry.Kuznecov <dimakuzn@gmail.com>
2024-09-26 16:42:12 +0300

    update development DB

--- app/OtusKde/Program.cs
+++ app/OtusKde/Program.cs
@@ -13,15 +13,20 @@ builder.Services
     .AddJsonOptions(opt => opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 
-var DB_HOST = Environment.GetEnvironmentVariable("DB_HOST");
-var DB_PORT = Environment.GetEnvironmentVariable("DB_PORT");
-var DB_NAME = Environment.GetEnvironmentVariable("DB_NAME");
-var DB_USER = Environment.GetEnvironmentVariable("DB_USER");
-var DB_PASSWORD = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var connectionString = "Host=localhost;Database=otus;Username=postgres;Password=postgres;Port=5432";
+if (!builder.Environment.IsDevelopment())
+{
+    var DB_HOST = Environment.GetEnvironmentVariable("DB_HOST");
+    var DB_PORT = Environment.GetEnvironmentVariable("DB_PORT");
+    var DB_NAME = Environment.GetEnvironmentVariable("DB_NAME");
+    var DB_USER = Environment.GetEnvironmentVariable("DB_USER");
+    var DB_PASSWORD = Environment.GetEnvironmentVariable("DB_PASSWORD");
+    connectionString =
+        $"Host={DB_HOST};Database={DB_NAME};Username={DB_USER};Password={DB_PASSWORD};Port={DB_PORT}";
+}
 
 builder.Services.AddDbContext<MasterContext>(
-    opt =>
-        opt.UseNpgsql($"Host={DB_HOST};Database={DB_NAME};Username={DB_USER};Password={DB_PASSWORD};Port={DB_PORT}")
+    opt => opt.UseNpgsql(connectionString)
 );
 
 var app = builder.Build();
